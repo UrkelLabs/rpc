8b2d4ef5f8298f3baabab5d587fd91b6c797e661
kilpatty <seanpkilgarriff@gmail.com>
2019-09-05 17:24:49 -0500

    json-client: expose Error

--- json-client/src/lib.rs
+++ json-client/src/lib.rs
@@ -3,5 +3,5 @@ pub mod error;
 pub mod types;
 
 pub use crate::client::RpcClient;
-pub use crate::error::RpcError;
+pub use crate::error::{Error, RpcError};
 pub use crate::types::{RpcRequest, RpcResponse};
